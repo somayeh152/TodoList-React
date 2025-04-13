340143a4433e61b9cc824982e2faaf5207fd131c
s.ebrahimzadeh <s.ebrahimzadeh@zi-tel.com>
2025-04-13 19:25:34 +0330

    added interceptors

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -6,6 +6,38 @@ const axiosInstance = axios.create({
         "Content-Type": "application/json",
     },
     timeout: 5000,
-})
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('auth-token');
+        if (token) {
+            config.headers.authorization = `Bearer ${token}`;
+        }
+        console.log('Request sent with token:', config.headers['Authorization']);
+        return config;
+    }, (error) => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        console.log('Response received:', response.data);
+        return response.data;
+    }, (error) => {
+        if (error.response) {
+            console.log('Error response:', error.response.status); // show error in UI
+            if (error.response.status === 401) {
+                console.log('Unauthorized! Redirecting to login...'); // redirect
+            }
+        } else if (error.request) {
+            console.log('No response received:', error.request);
+        } else {
+            console.log('Error setting up request:', error.message);
+        }
+        return Promise.reject(error);
+    });
 
 export default axiosInstance;
